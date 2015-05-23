33348442024c1712fe804914adc4458b1b64c4ce
CrashedBboy <crashedbboy@gmail.com>
2015-05-23 18:36:50 +0800

    Basic game struct and properties

--- src/layout.c
+++ src/layout.c
@@ -3,3 +3,7 @@ File		:layout.c
 Description	:arrange each static widget
 Date		:2015/5/22
 ***************************************/
+void layout_init(){
+
+}
+
--- src/property.c
+++ src/property.c
@@ -3,3 +3,22 @@ File		:property.c
 Description	:game parameter and constant
 Date		:2015/5/22
 ***************************************/
+struct block{
+	char top;
+	char right;
+	char bottom;
+	char left;
+};
+struct map{
+	struct block arrang[9];
+	char total_turns;
+	char default_blank_location;
+	char entry_block;
+	char entry_direction;
+	char exit_block;
+	char exit_direction;
+};
+static struct block now_arrange[9];
+static char blank_location;
+static char remain_turns;
+
